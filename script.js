2d11aff251f7e6065b8e9b41d3613f1c41ea6caa
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2025-10-05 21:41:01 -0300

    05/10

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 function mostraPalavrasChave() { 
   const texto = document.querySelector("#entrada-de-texto").value;
   const campoResultado = document.querySelector("#resultado-palavrachave");
-  const palavras = texto.split(" ");
+  const PalavrasChave = processaTexto(texto);
 
-  campoResultado.textContent = palavras.join(", ");
+  campoResultado.textContent = PalavrasChave.join(", ");
 } 
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
